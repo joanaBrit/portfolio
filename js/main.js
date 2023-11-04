3f1a69f0277eff3d59128ff4597cfa717b334393
joana <joanabrito216@gmail.com>
2023-11-04 00:07:56 +0000

    fixed the main issue and last stylings

--- js/main.js
+++ js/main.js
@@ -14,53 +14,39 @@ function scrollNav() {
 
 // //? scroll "github" horizontally
 
-const scrollSides = document.querySelectorAll('.scroll-side')
-let lastScrollPositions = new Array(scrollSides.length).fill(0)
-let isMoving = new Array(scrollSides.length).fill(false)
-// when github is stop color
-scrollSides.forEach((scrollSide, i) => {
+const SCROLL_BEFORE_AMOUNT = 550
 
-  scrollSide.addEventListener('transitionend', () => {
-    isMoving[i] = false
-    scrollSide.style.backgroundColor = 'transparent'
-    scrollSide.style.color = '#3C3C3C'
-  })
-})
 window.addEventListener('scroll', () => {
 
   const scrollY = window.scrollY
+  const sectionPositions = {
+    project4: document.querySelector('.project4').offsetTop,
+    project3: document.querySelector('.project3').offsetTop,
+    project2: document.querySelector('.project2').offsetTop,
+    project1: document.querySelector('.project1').offsetTop
+  }
 
-  scrollSides.forEach((scrollSide, i) => {
-    // track previous position
-    const scrollDelta = scrollY - lastScrollPositions[i]
-    lastScrollPositions[i] = scrollY
+  const projectClasses = ['project4', 'project3', 'project2', 'project1']
 
-    // Color when is moving
-    isMoving[i] = Math.abs(scrollDelta) > 0
+  projectClasses.forEach(project => {
 
-    // retrive the current transformation
-    const currentTransform = getComputedStyle(scrollSide).transform
+    const button = document.querySelector(`.${project} .scroll-side`)
 
-    if (currentTransform) {
-      const currentTranslateX = parseFloat(currentTransform.split(',')[4])
-      // maxium limit for horizontal move
-      const maxTranslateX = 200
-      // new horizontal position
-      const newTranslateX = currentTranslateX - scrollDelta
-      const finalTranslateX = Math.min(maxTranslateX, Math.max(4, newTranslateX))
-      // Make sure doesn't pass the limit
-      scrollSide.style.transform = `translateX(${finalTranslateX}px)`
-
-      if (isMoving[i]) {
-        scrollSide.style.backgroundColor = '#ff340adb'
-        scrollSide.style.color = '#f3f3f3'
+    if (scrollY > sectionPositions[project] - SCROLL_BEFORE_AMOUNT) {
+      if (scrollY > sectionPositions[project] + SCROLL_BEFORE_AMOUNT) {
+        button.classList.remove('show-up')
+        button.classList.add('show-down')
       } else {
-        scrollSide.style.backgroundColor = 'transparent'
-        scrollSide.style.color = '#3C3C3C'
+        button.classList.add('show-up')
+        button.classList.remove('show-down')
       }
+    } else {
+      button.classList.remove('show-down', 'show-up')
     }
   })
 })
+
+
 
 //? Title typing effect
 
@@ -80,3 +66,61 @@ function writeText() {
   }
 }
 intervalID = setInterval(writeText, 100)
+
+
+
+// *Experiences
+
+// const scrollSides = document.querySelectorAll('scroll-left')
+// let lastScrollPositions = new Array(scrollSides.length).fill()
+// let isMoving = new Array(scrollSides.length).fill(false)
+// when github is stop color
+// scrollSides.forEach((scrollSide, i) => {
+
+//   scrollSide.addEventListener('transitionend', () => {
+//     isMoving[i] = false
+//     scrollSide.style.backgroundColor = 'transparent'
+//     scrollSide.style.color = '#3C3C3C'
+//   })
+// })
+
+// SCROLL_BEFORE_AMOUNT > 1500
+// projectClasses.forEach(project => {
+//   if (scrollY > sectionDownPositions[project] - SCROLL_BEFORE_AMOUNT) {
+//     const button = document.get(`.${project} #rigth`)
+//     button.classList.remove('show')
+//     button.classList.add('hide')
+//   }
+// })
+
+// scrollSides.forEach((scrollSide, i) => {
+//   // track previous position
+//   const scrollDelta = scrollY - lastScrollPositions[i]
+//   lastScrollPositions[i] = scrollY
+
+//   // // Color when is moving
+//   // isMoving[i] = Math.abs(scrollDelta) > 0
+
+//   // retrive the current transformation
+//   const currentTransform = getComputedStyle(scrollSide).transform
+
+//   if (currentTransform) {
+//     const currentTranslateX = parseFloat(currentTransform.split(',')[4])
+//     // maxium limit for horizontal move
+//     const maxTranslateX = 200
+//     // new horizontal position
+//     const newTranslateX = currentTranslateX - scrollDelta
+//     const finalTranslateX = Math.min(maxTranslateX, Math.max(0, newTranslateX))
+//     // Make sure doesn't pass the limit
+//     scrollSide.style.transform = `translateX(${finalTranslateX}px)`
+
+//     // if (isMoving[i]) {
+//     //   scrollSide.style.backgroundColor = '#ff340adb'
+//     //   scrollSide.style.color = '#f3f3f3'
+//     // } else {
+//     //   scrollSide.style.backgroundColor = 'transparent'
+//     //   scrollSide.style.color = '#3C3C3C'
+//     // }
+//   }
+// })
+// })
